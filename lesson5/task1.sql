789d6d6d11e4945d88b166fd1ff7034e0b13fc76
Gwellir <gwellir@gmail.com>
2019-11-07 01:39:46 +0300

    Update task1.sql

--- lesson5/task1.sql
+++ lesson5/task1.sql
@@ -1,5 +1,8 @@
+-- Задача 02
+/* Пусть в таблице users поля created_at и updated_at оказались
+незаполненными. Заполните их текущими датой и временем. */
+
 USE my_table;
 UPDATE users
-  SET created_at = now();
-UPDATE users 
-  SET updated_at = now();
+SET created_at = now(),
+    updated_at = now();
