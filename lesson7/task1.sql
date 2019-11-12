793e8c4a8cf6e18f1b63daa278d847d0ea931dc4
Gwellir <gwellir@gmail.com>
2019-11-12 15:12:55 +0300

    Create task1.sql

---
+++ lesson7/task1.sql
@@ -0,0 +1,4 @@
+-- Задача 7.1
+/* Составьте список пользователей users, которые осуществили хотя бы один заказ orders в интернет магазине. */
+
+SELECT name FROM users WHERE users.id IN (SELECT user_id FROM orders);
